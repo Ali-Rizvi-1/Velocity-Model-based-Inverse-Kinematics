f875106d5ab3833b7f49b23527aa55c84f594249
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-10-15 15:37:54 +0500

    Modified

--- simulation.m
+++ simulation.m
@@ -1,9 +1,9 @@
 % velocity motion model based inverse kinematics
 % (c) 
 
-clear all;clc;
-T =0.1;
-ITER = 1e3;
+clear;clc; close all;
+T =0.01;
+ITER = 1000;
 L = 0.5; %robot width
 r = 0.25; %wheel radius
 
@@ -11,15 +11,18 @@ t=linspace(-pi,pi,ITER);
 xt = 8*sin(t).^3; yt = 8*sin((2*t)).^3;
 %plot(x,y)
 
-N=0:1:ITER;
+N=0:ITER;
 x = 8*(sin(N*T)).^3;
 y = 8*(sin(2*N*T)).^3;
 %figure;
-plot(x,y)
+plot(x,y, 'linewidth', 3)
+title('Original Discrete Trajectory')
 figure;
 
 % initializing
 phiN = zeros(1, ITER); w = zeros(1, ITER); v = zeros(1, ITER);
+v_r = zeros(1, ITER); v_l = zeros(1, ITER); w_l = zeros(1, ITER); 
+w_r = zeros(1, ITER);
 
 for n=2:length(N)-1
     phiN(n) = atan2(y(n) - y(n-1), x(n) - x(n-1));
@@ -30,8 +33,8 @@ for n=2:length(N)-1
     xm = 0.5*(x(n)+x(n+1));
     ym = 0.5*(y(n)+y(n+1));
     
-    xs = xm + meu*(y(n)-y(n+1));
-    ys = ym + meu*(x(n+1)-x(n));
+    xs = xm - (meu/2)*(y(n+1)-y(n));
+    ys = ym + (meu/2)*(x(n+1)-x(n));
 
     R = sqrt((x(n)-xs)^2+(y(n)-ys)^2);
     
@@ -42,16 +45,29 @@ for n=2:length(N)-1
     
     w(n) = delta_phi_n / T;
     
-    v(n) = R*w(n);
+    v(n) = R*abs(w(n));
+    
+    v_r(n) = ( (R + 0.5*L).*w(n)); % right wheel linear velocity
+    w_r(n) = v_r(n)/ r; % right wheel angular velocity
+
+    v_l(n) = ( (R - 0.5*L).*w(n)); % left wheel linear velocity
+    w_l(n) = v_l(n)/r; % left wheel angular velocity
     
 end 
-R_t = v./w; 
 
-v_r = ( (R_t + 0.5*L).*w); % right wheel linear velocity
-w_r = v_r ./ r; % right wheel angular velocity
+subplot 211
+plot(v); title('linear velocity')
+subplot 212
+plot(w); title('anguar velocity')
+figure;
 
-v_l = ( (R_t - 0.5*L).*w); % left wheel linear velocity
-w_l = v_l./r; % left wheel angular velocity
+% R_t = v./w; 
+% 
+% v_r = ( (R_t + 0.5*L).*w); % right wheel linear velocity
+% w_r = v_r ./ r; % right wheel angular velocity
+% 
+% v_l = ( (R_t - 0.5*L).*w); % left wheel linear velocity
+% w_l = v_l./r; % left wheel angular velocity
 
 subplot 221
 plot(w_r); title('w_r')
@@ -64,9 +80,16 @@ plot(v_l); title('v_l')
 
 
 x_f = zeros(1, ITER); y_f = zeros(1, ITER); phi_f = zeros(1, ITER); 
-for n = 2:ITER-1
-    x_f(n+1) = x_f(n) + (v(n)/w(n))*(-sin(phiN(n))+sin(phiN(n)+w(n)*T));
-    y_f(n+1) = y_f(n) + (v(n)/w(n))*(-cos(phiN(n))+cos(phiN(n)+w(n)*T));
+for n = 1:ITER-1
+%     x_f(n+1) = x_f(n) + (v(n)/w(n))*(-sin(phiN(n))+sin(phiN(n)+w(n)*T));
+%     y_f(n+1) = y_f(n) + (v(n)/w(n))*(-cos(phiN(n))+cos(phiN(n)+w(n)*T));
+    
+    x_f(n+1) = x_f(n) + v(n)*T*cos(phiN(n));
+    y_f(n+1) = y_f(n) + v(n)*T*sin(phiN(n));
+    
+    %phiN(n+1) = phiN(n)+T*w(n);
+
 end
 figure;
 plot(x_f,y_f)
+title('Recovered trajectory')
